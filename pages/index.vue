e3c07f1bbb05b4d193893dff5cfc7a1cfcadf180
am-765 <zjhghjnlzbghh.765+github@gmail.com>
2021-03-04 12:53:00 +0900

    test変更

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,9 @@
       <Logo />
       <h1 class="title">
         test-project
+        testtesttest
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+
     </div>
   </div>
 </template>
